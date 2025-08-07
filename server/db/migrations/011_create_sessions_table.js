22fb64f2bb8f310d56176ecf446e3de98fce1fba
airbomberofaqua <airbomberofaqua@gmail.com>
2025-08-07 14:15:25 -0400

    Fix migration numbering and add missing sessions table migration

---
+++ server/db/migrations/011_create_sessions_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('sessions', function(table) {
+    table.string('session_id').primary();
+    table.json('sess').notNull();
+    table.timestamp('expired').notNull().index();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('sessions');
+};
